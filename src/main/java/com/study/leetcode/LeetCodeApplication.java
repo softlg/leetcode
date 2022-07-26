a839e6b6459f1ce1679f9a2b579f9553aa90dbba
softlg@163.com <softlg>
2022-07-26 15:46:37 +0800

    init commit

---
+++ src/main/java/com/study/leetcode/LeetCodeApplication.java
@@ -0,0 +1,13 @@
+package com.study.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/leetcode/LeetCodeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.study.leetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("aaaaaaaaaaaaaa");
+    }
+
+}
