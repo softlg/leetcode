7537c8e26659495b4f18d773d4a8ab8e3e83e8e9
c <softlg@163.com>
2022-08-03 14:59:01 +0800

    Feat:T235_二叉搜索树的最近公共祖先

---
+++ src/main/java/com/study/leetcode/August/T235_二叉搜索树的最近公共祖先.java
@@ -0,0 +1,35 @@
+package com.study.leetcode.August;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @author c
+ * 2022/8/3
+ */
+public class T235_二叉搜索树的最近公共祖先 {
+     /*
+    难度：中等
+    --------------------------------------------------------------------------------
+    题目：
+       给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+
+       百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，
+       满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+
+    --------------------------------------------------------------------------------
+    解题思路：
+
+
+    --------------------------------------------------------------------------------
+     */
+
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if((root.val - p.val)*(root.val - q.val) <= 0){
+//            return root;
+//        }
+//        root = p.val < root.val ? root.left : root.right;
+//        return lowestCommonAncestor(root,p,q);
+//    }
+
+}
