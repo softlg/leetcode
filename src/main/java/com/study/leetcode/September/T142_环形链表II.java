99622a83919d77bd95a05d596f5fa5e1b752878f
softlg <2013145231@qq.com>
2022-09-20 21:03:43 +0800

    Feat：T142_环形链表II

---
+++ src/main/java/com/study/leetcode/September/T142_环形链表II.java
@@ -0,0 +1,47 @@
+package com.study.leetcode.September;
+
+import com.study.leetcode.util.ListNode;
+
+/**
+ * @author：c
+ * Date：2022/9/20
+ */
+public class T142_环形链表II {
+    /*
+    难度：中等
+    --------------------------------------------------------------------------------
+    题目：
+        给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+
+        如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用
+        整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参
+        数进行传递，仅仅是为了标识链表的实际情况。
+
+        不允许修改 链表。
+    --------------------------------------------------------------------------------
+    解题思路：
+
+    --------------------------------------------------------------------------------
+     */
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (true) {
+            if (fast == null || fast.next == null){
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                break;
+            }
+        }
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
+
+}
