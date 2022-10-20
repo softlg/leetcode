939dfa45f703f72ce7812152cb4e10603fca515d
ext.liaog1 <ext.liaog1@jd.com>
2022-10-20 16:56:15 +0800

    Feat:T144_二叉树的前序遍历 和 自定义 TreeNode

---
+++ src/main/java/com/study/leetcode/util/TreeNode.java
@@ -0,0 +1,23 @@
+package com.study.leetcode.util;
+
+/**
+ * @author： liaoguang
+ * @date： 2022/10/20 11:46
+ * Description：
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode() {}
+
+     public TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
