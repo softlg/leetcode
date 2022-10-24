42d359374af2a35fc6c75809da73fbbfe2facdb1
ext.liaog1 <ext.liaog1@jd.com>
2022-10-24 17:56:52 +0800

    Feat:T101_对称二叉树 初始化

---
+++ src/main/java/com/study/leetcode/October/T101_对称二叉树.java
@@ -0,0 +1,35 @@
+package com.study.leetcode.October;
+
+import com.study.leetcode.util.TreeNode;
+
+/**
+ * @author： liaoguang
+ * @date： 2022/10/24 11:37
+ * Description：
+ */
+public class T101_对称二叉树 {
+    /*
+    难度：easy
+    --------------------------------------------------------------------------------
+    题目：
+          给你一个二叉树的根节点 root ， 检查它是否轴对称。
+
+      输入：root = [1,2,2,3,4,4,3]
+      输出：true
+    --------------------------------------------------------------------------------
+    解题思路：
+
+    --------------------------------------------------------------------------------
+    */
+
+    public boolean isSymmetric(TreeNode root) {
+
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
