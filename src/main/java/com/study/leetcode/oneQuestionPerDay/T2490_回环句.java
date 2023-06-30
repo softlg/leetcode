522731e5b75f89578871896a98e82f17b825bd3b
liaoguang <guang.liao@esrgear.com>
2023-06-30 16:31:21 +0800

    feat:T2490_回环句

---
+++ src/main/java/com/study/leetcode/oneQuestionPerDay/T2490_回环句.java
@@ -0,0 +1,39 @@
+package com.study.leetcode.oneQuestionPerDay;
+
+public class T2490_回环句 {
+    /*
+    难度：easy
+    --------------------------------------------------------------------------------
+    题目：
+      句子 是由单个空格分隔的一组单词，且不含前导或尾随空格。
+
+        例如，"Hello World"、"HELLO"、"hello world hello world" 都是符合要求的句子。
+        单词 仅 由大写和小写英文字母组成。且大写和小写字母会视作不同字符。
+        如果句子满足下述全部条件，则认为它是一个 回环句 ：
+
+        单词的最后一个字符和下一个单词的第一个字符相等。
+        最后一个单词的最后一个字符和第一个单词的第一个字符相等。
+        例如，"leetcode exercises sound delightful"、"eetcode"、"leetcode eats soul" 都是回环句。
+        然而，"Leetcode is cool"、"happy Leetcode"、"Leetcode" 和 "I like Leetcode" 都 不 是回环句。
+
+        给你一个字符串 sentence ，请你判断它是不是一个回环句。如果是，返回 true ；否则，返回 false 。
+    --------------------------------------------------------------------------------
+
+    解题思路：
+           字符串
+    --------------------------------------------------------------------------------
+     */
+
+    public boolean isCircularSentence(String s) {
+        int n = s.length();
+        if (s.charAt(0) != s.charAt(n - 1)) {
+            return false;
+        }
+        for (int i = 1; i < n; ++i) {
+            if (s.charAt(i) == ' ' && s.charAt(i - 1) != s.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
