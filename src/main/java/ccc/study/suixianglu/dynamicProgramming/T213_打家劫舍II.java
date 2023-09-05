4ba0b8d65ac0b3af4d102de3bd125a674f1e91bd
liaoguang <guang.liao@esrgear.com>
2023-09-05 16:37:46 +0800

    feat:T213_打家劫舍II

---
+++ src/main/java/ccc/study/suixianglu/dynamicProgramming/T213_打家劫舍II.java
@@ -0,0 +1,42 @@
+package ccc.study.suixianglu.dynamicProgramming;
+
+/**
+ * @author c
+ * @date 2023-9-5 09:37:13
+ */
+public class T213_打家劫舍II {
+    /*
+    难度：medium
+    --------------------------------------------------------------------------------
+    题目：
+        你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，
+        这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，
+        如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+
+        给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+    --------------------------------------------------------------------------------
+    解题思路：
+        dp
+    --------------------------------------------------------------------------------
+     */
+
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int len = nums.length;
+        if (len == 1)
+            return nums[0];
+        return Math.max(robAction(nums, 0, len - 1), robAction(nums, 1, len));
+    }
+
+    int robAction(int[] nums, int start, int end) {
+        int x = 0, y = 0, z = 0;
+        for (int i = start; i < end; i++) {
+            y = z;
+            z = Math.max(y, x + nums[i]);
+            x = y;
+        }
+        return z;
+    }
+
+}
